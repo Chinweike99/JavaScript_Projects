b87a70a4bf7e6f125d62b54a3798296139b88d61
Chinweike99 <Innocentakwolu1@gmail.com>
2024-03-03 09:10:47 -0800

    updated

---
+++ QR_Code_generator/script.js
@@ -0,0 +1,20 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+
+
+const apiURL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+function genarateQr(){
+    if(qrText.value.length > 0){
+        qrImage.src = apiURL + qrText.value;
+        imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+
+}
